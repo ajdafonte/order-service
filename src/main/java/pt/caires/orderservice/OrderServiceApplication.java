bb28024cc5bff3dcd9096b8e133c07ecc6b9e4ac
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2022-09-02 10:09:44 +0200

    First commit

---
+++ src/main/java/pt/caires/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package pt.caires.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrderServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pt/caires/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.caires.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
